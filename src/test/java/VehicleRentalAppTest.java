4314478c027acd2c2ffd7cf28ffa1c988c599c53
TheIkerOne <ikeralvaign@gmail.com>
2026-03-28 17:46:41 -0400

    Add testRentAndReturnVehicle test method, testing the creation and correct functioning of a vehicle object when rented and returned.

--- src/test/java/VehicleRentalAppTest.java
+++ src/test/java/VehicleRentalAppTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 class VehicleRentalAppTest {
     @Test
     public void testLicensePlate() {
@@ -33,4 +35,27 @@ class VehicleRentalAppTest {
             vehicle4.setLicensePlate("ZZZ99");
         });
     }
+    
+    @Test
+    public void testRentAndReturnVehicle() {
+		// Initializes the single RentalSystem object, as well as the vehicle and customer objects.
+		RentalSystem testSystem = RentalSystem.getInstance();
+		Vehicle testVehicle = new Car("Porsche", "911", 2023, 4);
+		Customer testCustomer = new Customer(2342532, "Joey");
+		// Tests whether the objects initialize with expected properties, and get added to the system correctly
+		assertEquals(Vehicle.VehicleStatus.Available, testVehicle.getStatus());
+		assertTrue(testSystem.addVehicle(testVehicle));
+		assertTrue(testSystem.addCustomer(testCustomer));
+		LocalDate testDate = LocalDate.of(2025, 11, 29);
+		boolean rentResult = testSystem.rentVehicle(testVehicle, testCustomer, testDate, 1000);
+		assertTrue(rentResult);
+		assertEquals(Vehicle.VehicleStatus.Rented, testVehicle.getStatus());
+		rentResult = testSystem.rentVehicle(testVehicle, testCustomer, testDate, 1000);
+		assertFalse(rentResult);
+		boolean returnResult = testSystem.returnVehicle(testVehicle, testCustomer, testDate, 500);
+		assertTrue(returnResult);
+		assertEquals(Vehicle.VehicleStatus.Available, testVehicle.getStatus());
+		returnResult = testSystem.returnVehicle(testVehicle, testCustomer, testDate, 500);
+		assertFalse(returnResult);
+	}
 }
